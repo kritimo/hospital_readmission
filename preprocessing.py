0abfc6dc582439aec09ce75753c6b1e8783e9c12
Kriti Moogala <kmoogala@umich.edu>
2024-08-23 12:19:18 -0400

    dropping incomplete data

--- preprocessing.py
+++ preprocessing.py
@@ -5,5 +5,8 @@ from statistics import mode
 import matplotlib.pyplot as plt  
 from sklearn.preprocessing import MinMaxScaler
 
+# based on a project:
+# https://github.com/u-raza/readmission-prediction/tree/master
+
 df_original = pd.read_csv("diabetic_data.csv")
 print(df_original.shape)
